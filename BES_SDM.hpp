ae84109940f504ca63938d84ab03487d97b432e1
Ninjasaurio <fernando.elena14072@gmail.com>
2025-01-20 09:31:39 +0100

    documentación doxygen acabada

--- BES_SDM.hpp
+++ BES_SDM.hpp
@@ -7,17 +7,17 @@
 /**
  *@brief struct representing a subset group in the SDM scheme
  *
-*/
-typedef struct key_subset{
-	unsigned int high_node;
-	unsigned int low_node;
-}Key_subset;
-
+ */
+typedef struct key_subset
+{
+    unsigned int high_node;
+    unsigned int low_node;
+} Key_subset;
 
 /**
  *@brief params used by get_allowed_keys to diferentiate between nodes
  *
-*/
+ */
 const char O_node = 0; // Operative user
 const char D_node = 1; // Denied user
 const char S_node = 2; // Semi operative node
@@ -26,42 +26,42 @@ const char S_node = 2; // Semi operative node
  * @class BES_SDM_scheme
  * @brief Class representing a Subset Difference Broadcast Encryption Scheme (BES) which inherits from Keytree.
  */
-class BES_SDM_scheme : public Keytree {
+class BES_SDM_scheme : public Keytree
+{
 private:
-    /**
-     * @brief Auxiliary method to find the current allowed keys in the tree starting from a given index.
-     * 
+    /*!
+     * \brief Finds the path between a leaf and a node.
+     *
+     * \param leaf_node_index The index of the leaf node.
+     * \param root_node_index The index of the root node.
+     * \param path Vector to store the path.
+     * \return 1 if the path is found, -1 if the leaf and root nodes are the same.
      */
-    void find_allowed_keys(vector<unsigned int>& node_key_ID, vector<uint8_t*>& user_keys, unsigned int index);
+    int find_path(unsigned int leaf_node_index, unsigned int root_node_index, vector<unsigned int> &path);
 
-	/**
-	 *@brief find the path in the tree between a leaf and a subtree root node
-	 *
-	*/
-	int find_path(unsigned int leaf_node_index, unsigned int root_node_index , vector<unsigned int>& path); 
+    /*!
+     * \brief Generates a triple-sized key using a Deterministic Random Byte Generator (DRBG).
+     *
+     * \param key_in The input key.
+     * \param key_size The size of the input key.
+     * \param triple_out Buffer to store the triple-sized key output.
+     */
+    void drbg_triplesize(uint8_t *key_in, size_t key_size, uint8_t *triple_out);
 
-	/**
-	 *@brief calculates the correspondent key for a subset of the SDM BES
-	 *
-	*/
-	int calculate_subset_key(unsigned int i,unsigned int j , uint8_t *key);
-
-	/**
-	 *@brief triples the size of a key with a DRBG based on AES256-CTR
-	 *
-	*/
-	void drbg_triplesize(uint8_t *key_in,size_t key_size, uint8_t *triple_out);
-
-	/**
-	 *@brief given a subtree root node of an already created subtree created by get_allowed_key, finds the corresponding subset
-	 *
-	*/
-	Key_subset find_subset_and_key(int subtree_root_node, vector <char> node_tree,uint8_t *key);
+    /*!
+     * \brief Finds the subset and key for a given subtree in the node tree.
+     *
+     * \param subtree_root_node The index of the subtree root node.
+     * \param node_tree Vector representing the structure of the node tree.
+     * \param key Buffer to store the derived key.
+     * \return An instance of Key_subset containing the high and low nodes of the subset.
+     */
+    Key_subset find_subset_and_key(int subtree_root_node, vector<char> node_tree, uint8_t *key);
 
 public:
     /**
      * @brief Constructor for a Subset Difference BES scheme.
-     * 
+     *
      * @param Tree_Depth The depth of the tree.
      * @param node_key_length The length of the node keys in bits.
      */
@@ -72,28 +72,33 @@ public:
      */
     ~BES_SDM_scheme() = default;
 
-    /**
-     * @brief Deny access for keys to a user.
-     * 
-     * @param userID The ID of the user to be denied access.
-     * @return 1 if the user is successfully denied, -1 if the user ID is invalid.
+    /*!
+     * @brief Denies access to a user by their user ID.
+     *
+     * @param userID The ID of the user.
+     * @return 1 if the user access is successfully denied, -1 if the user ID is invalid.
+     * @throws invalid_argument if the user ID is invalid.
      */
     int denegate_user(unsigned int userID);
 
-    /**
-     * @brief Get the corresponding keys for a determined user.
-     * 
+    /*!
+     * @brief Gets the keys for a specific user.
+     *
+     * @param userID The ID of the user.
+     * @param user_keys_id Vector to store the user's key subset IDs.
+     * @param user_keys Vector to store the user's keys.
+     * @return 1 if the user keys are successfully retrieved, -1 if the user ID is invalid.
+     * @throws invalid_argument if the user ID is invalid.
      */
-    int get_user_keys(unsigned int userID, vector<Key_subset>& user_keys_id, vector<uint8_t*>& user_keys);
+    int get_user_keys(unsigned int userID, vector<Key_subset> &user_keys_id, vector<uint8_t *> &user_keys);
 
-    /**
-     * @brief Get the allowed keys that can be used at the moment with the currently allowed users.
-     * 
+    /*!
+     * @brief Gets the allowed keys for operative users in the system.
+     *
+     * @param user_keys_id Vector to store the key subset IDs.
+     * @param user_keys Vector to store the keys.
      */
-    void get_allowed_keys(vector<Key_subset>& user_keys_id, vector<uint8_t*>& user_keys);
+    void get_allowed_keys(vector<Key_subset> &user_keys_id, vector<uint8_t *> &user_keys);
 };
 
 #endif
-
-
-
