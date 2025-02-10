d55033ef2e30ff123896bf273a7601631381f8a4
Ninjasaurio <fernando.elena14072@gmail.com>
2025-02-10 10:07:45 +0100

    mejorando el main de pruebas

--- testing_main.cpp
+++ testing_main.cpp
@@ -1,4 +1,4 @@
-// compilar el código con g++ y la opción -maes para indicar al compilador que utilize las intrucciones de INTEL y AMD de AES
+// compile the code with g++ and the -maes option to tell the compiler to use the INTEL and AMD AES instructions
 // g++ BES_SDM.cpp BES_CSM.cpp DRBG_AES.cpp testing_main.cpp Key_Tree.cpp -maes
 
 #include "Key_Tree.hpp"
@@ -7,70 +7,97 @@
 
 using namespace std;
 
+//this main will be used to informally unit test the BES_CSM and BES_SDM schemes, it have memory leaks in key vectors, but does not matter because
+//it is only for testing purposes
+
+//function to write a message in one color
+const string GREEN = "\033[32m";
+const string RED = "\033[31m";
+const string BLUE = "\033[34m";
+const string BLUE_CYAN = "\033[36m";
+
+
+void print_color(string message , string color){
+	cout << color << message << "\033[37m" << endl;
+}
+
+void print_keys_CSM(vector <unsigned int> key_index ,vector <uint8_t*>keys_vector,size_t key_size){
+	for(int i = 0 ; i < keys_vector.size() ; i++){
+		cout << "key index: " << key_index[i] << " KEY:";
+		printHex(keys_vector[i],key_size/8);
+	}
+}
+
+void print_keys_SDM(vector <Key_subset> key_index ,vector <uint8_t*>keys_vector,size_t key_size){
+	for(int i = 0 ; i < keys_vector.size() ; i++){
+		cout << "key index high: " << key_index[i].high_node <<" ,key index low: " <<key_index[i].low_node <<  " KEY:";
+		printHex(keys_vector[i],key_size/8);
+	}
+}
+
 int main(){
-	/*BES_CSM_scheme BES_SDM_Tree_1(3 ,256);
+	//testing variables
+	vector <uint8_t*> user_keys_CSM;
+	vector <unsigned int> key_indexes_CSM;
 
-	vector <unsigned int> user_keys_id;
-	vector <uint8_t*> user_keys;
-	BES_SDM_Tree_1.denegate_user(0);
-	BES_SDM_Tree_1.denegate_user(1);
-	BES_SDM_Tree_1.denegate_user(4);
-	BES_SDM_Tree_1.denegate_user(7);
+	/////////////////////////////////////////CSM SCHEME INFORMAL TESTS////////////////////////////////////////////////
+
+	//Creating a new CSM scheme
+	print_color("CSM SCHEME UNITARY TESTING",RED);
+	print_color("creating a CSM scheme Key_tree",BLUE_CYAN);
+	BES_CSM_scheme CSM_scheme(3,256); // 3 is the depth of the tree, 256 is the key length in bits
+	print_color("the content of the Keytree is: ",BLUE_CYAN);
+	CSM_scheme.print_KeyTree_info();
+	print_color("Current allowed keys are: ",BLUE_CYAN);
+	CSM_scheme.get_allowed_keys(key_indexes_CSM,user_keys_CSM);
+	print_keys_CSM(key_indexes_CSM,user_keys_CSM,256);
+
+	//check functionality deny users
+	user_keys_CSM.clear();
+	key_indexes_CSM.clear();
+	print_color("deniying some users to check deny funcionality:",BLUE_CYAN);
+	CSM_scheme.denegate_user(1); // deny user with id 1
+	CSM_scheme.denegate_user(2); // deny user with id 2
+	CSM_scheme.denegate_user(7); // deny user with id 7
+	print_color("The current denied users are:",BLUE_CYAN);
+	CSM_scheme.print_KeyTree_info();
+	print_color("Current allowed keys are: ",BLUE_CYAN);
+	CSM_scheme.get_allowed_keys(key_indexes_CSM,user_keys_CSM);
+	print_keys_CSM(key_indexes_CSM,user_keys_CSM,256);
+
+	//check funcionality get keys for a user
+	user_keys_CSM.clear();
+	key_indexes_CSM.clear();
+	CSM_scheme.get_user_keys_CSM(0,key_indexes_CSM,user_keys_CSM);
+	print_color("Keys for user 0 are:",BLUE_CYAN);
+	print_keys_CSM(key_indexes_CSM,user_keys_CSM,256);
+
+	//check functionality store and load a CSM scheme
+	ofstream ofs("CSM_scheme.dat",ios::binary);
+	ofs << CSM_scheme;
+	ofs.close();
+
+	BES_CSM_scheme LOAD_CSM_scheme(3,256);
+	ifstream ifs("CSM_scheme.dat",ios::binary);
+	ifs >> LOAD_CSM_scheme;
+	ofs.close();
+	print_color("CSM KeyTree after store and load in different object is: ",BLUE_CYAN);
+	LOAD_CSM_scheme.print_KeyTree_info();
+	print_color("END OF CSM SCHEME TESTING ",GREEN);
+	cout << endl << endl;
+
+	/////////////////////////////////////////SDM SCHEME INFORMAL TESTS////////////////////////////////////////////////
+
+	//testing variables
+	vector <uint8_t*> user_keys_CSM;
+	vector <Key_subset> key_indexes_CSM;
+
+	//Creating a new SDM scheme
+	print_color("SDM SCHEME UNITARY TESTING",RED);
 
 
-	BES_SDM_Tree_1.print_KeyTree_info();
-	BES_SDM_Tree_1.get_user_keys(2,user_keys_id,user_keys);
-	
-	cout << "el usuario  tiene acceso a las siguientes claves:" << endl;
-	for(int i = 0 ; i < user_keys_id.size() ; i++){
-		cout << "clave con id: " << user_keys_id[i] << " con valor: ";
-		printHex(user_keys[i],32);
-		
-	}
-	user_keys_id.clear();
-	user_keys.clear();
 
-	BES_SDM_Tree_1.get_allowed_keys(user_keys_id,user_keys);
-	cout << "las claves que van a ser utilizadas son: " << endl;
-	cout << "el tamaño de el vector de claves es: " << user_keys_id.size() << endl;
-
-	for(int i = 0; i < user_keys_id.size(); i++){
-		cout << "clave con id: " << user_keys_id[i] << " y valor: ";
-		printHex(user_keys[i],32);
-	}
-
-	user_keys.clear();
-	BES_SDM_scheme BES_SDM_Tree_1(4 , 256);
-
-	BES_SDM_Tree_1.denegate_user(0);
-	BES_SDM_Tree_1.denegate_user(2);
-	BES_SDM_Tree_1.denegate_user(4);
-	BES_SDM_Tree_1.denegate_user(8);
-
-
-	BES_SDM_Tree_1.print_KeyTree_info();
-	
-	vector <Key_subset> key_indexes;
-	BES_SDM_Tree_1.get_user_keys(2,key_indexes,user_keys);
-	
-	cout << "el usuario tiene acceso a los labels:" << endl;
-
-	for(int i = 0 ; i < key_indexes.size() ; i++){
-		cout << "index i = " << key_indexes[i].high_node <<" index j = " << key_indexes[i].low_node << "  key : ";
-		printHex(user_keys[i],32);
-	}
-
-	key_indexes.clear();
-	user_keys.clear();
-	BES_SDM_Tree_1.get_allowed_keys(key_indexes,user_keys);
-
-	cout << "las claves que van a ser utilizadas son:" << endl;
-	for(int i = 0 ; i < key_indexes.size() ; i++){
-		cout << "index i = " << key_indexes[i].high_node <<" index j = " << key_indexes[i].low_node << "  key : ";
-		printHex(user_keys[i],32);
-	}
-	*/
-	vector <uint8_t*> user_keys;
+	/*vector <uint8_t*> user_keys_CSM;
 	vector <key_subset> key_indexes;
 
 	BES_SDM_scheme BES_SDM_Tree_1(3, 256);
@@ -78,22 +105,22 @@ int main(){
 
     BES_SDM_Tree_1.print_KeyTree_info();
 
-	BES_SDM_Tree_1.get_user_keys(0,key_indexes,user_keys);
+	BES_SDM_Tree_1.get_user_keys_CSM(0,key_indexes,user_keys_CSM);
 	
 	cout << "el usuario tiene acceso a los labels:" << endl;
 
 	for(int i = 0 ; i < key_indexes.size() ; i++){
 		cout << "index i = " << key_indexes[i].high_node <<" index j = " << key_indexes[i].low_node << "  key : ";
-		printHex(user_keys[i],32);
+		printHex(user_keys_CSM[i],32);
 	}
 	key_indexes.clear();
-	user_keys.clear();
-	BES_SDM_Tree_1.get_allowed_keys(key_indexes,user_keys);
+	user_keys_CSM.clear();
+	BES_SDM_Tree_1.get_allowed_keys(key_indexes,user_keys_CSM);
 
 	cout << "las claves que van a ser utilizadas son:" << endl;
 	for(int i = 0 ; i < key_indexes.size() ; i++){
 		cout << "index i = " << key_indexes[i].high_node <<" index j = " << key_indexes[i].low_node << "  key : ";
-		printHex(user_keys[i],32);
+		printHex(user_keys_CSM[i],32);
 	}
 
 	ofstream ofs("mi_arbol.dat",ios::binary);
@@ -106,26 +133,26 @@ int main(){
 	BES_SDM_Tree_2.print_KeyTree_info();
 
 	key_indexes.clear();
-	user_keys.clear();
-	BES_SDM_Tree_2.get_user_keys(0,key_indexes,user_keys);
+	user_keys_CSM.clear();
+	BES_SDM_Tree_2.get_user_keys_CSM(0,key_indexes,user_keys_CSM);
 	
 	cout << "el usuario tiene acceso a los labels:" << endl;
 
 	for(int i = 0 ; i < key_indexes.size() ; i++){
 		cout << "index i = " << key_indexes[i].high_node <<" index j = " << key_indexes[i].low_node << "  key : ";
-		printHex(user_keys[i],32);
+		printHex(user_keys_CSM[i],32);
 	}
 	key_indexes.clear();
-	user_keys.clear();
-	BES_SDM_Tree_2.get_allowed_keys(key_indexes,user_keys);
+	user_keys_CSM.clear();
+	BES_SDM_Tree_2.get_allowed_keys(key_indexes,user_keys_CSM);
 
 	cout << "las claves que van a ser utilizadas son:" << endl;
 	for(int i = 0 ; i < key_indexes.size() ; i++){
 		cout << "index i = " << key_indexes[i].high_node <<" index j = " << key_indexes[i].low_node << "  key : ";
-		printHex(user_keys[i],32);
+		printHex(user_keys_CSM[i],32);
 	}
 
-
+	*/
 
     return 0;
 }
